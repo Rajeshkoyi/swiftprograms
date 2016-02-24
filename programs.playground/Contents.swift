f4197fe04e3467c96f670677300a470ba9bd41d8
Rajesh K <koyi.rajesh60@gmail.com>
2016-02-24 17:17:22 +0530

    point

--- programs.playground/Contents.swift
+++ programs.playground/Contents.swift
@@ -1,19 +1,48 @@
+
+
 //: Playground - noun: a place where people can play
+
+
+
 import Foundation
+
 struct Point {
+    
     var x:Double
+    
     var y:Double
     
-    func distanceTo(point point:Point)->Double {
-        print(self.x,point.x)
+    
+    mutating func moveBy(pointB:Point) {
+        
+        self.x = pointB.x
+        self.y = pointB.y
+    }
+    
+    func distanceTo(point:Point)->Double {
+        
+        
         let a=abs(self.x-point.x)
+        
         let b=abs(self.y-point.y)
+        
         let c=sqrt(a*a+b*b)
+        
         return c
         
     }
 }
 
-let pointA=Point(x: 1.0, y: 2.0)
-let pointB=Point(x: 4.0, y: 6.0)
-let distance=pointA.distanceTo(point: pointB)
+var pointA=Point(x: 1.0, y: 2.0)
+
+var pointB=Point(x: 3.5, y: 4.5)
+
+
+pointA.distanceTo(pointB)
+
+pointA.moveBy(pointB)
+
+var pointC=Point(x: 4.2, y: 5.3)
+
+pointA.distanceTo(pointC)
+
